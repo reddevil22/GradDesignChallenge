0870744dd8132d85e9907246ac0069adc46ae48c
Joel Kenneth Fredericks <joel.fredericks@kurtosys.com>
2018-02-05 10:38:08 +0200

    Added CSS, HTML and JS files to repo

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+/*
+The javascript file should hide/display content when the button is clicked as per the example given.
+Use straight javascript and avoid JQuery. You can use any resource to set this up. 
+Some hints are given below but you can use any structure or approach of your choosing.
+
+*/
+
+var button = document.querySelector('.targetClassName');
+
+button.addEventListener('click', function(e) {
+
+  
+});
